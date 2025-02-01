f80ab578bcec5b6c03c314c4d7f72f7020a78abd
pateldeev25-pated201 <pateldeev25@gmail.com>
2025-02-01 13:44:55 -0500

    Setup

---
+++ binks/src/test/java/com/dylanmkdr/gdsc2025/binks/BinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylanmkdr.gdsc2025.binks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
